a57a24e66ca2db656371f03948f527df8d5aecb5
Toby <tobylobytongalong@hotmail.com>
2016-05-27 02:48:41 +1200

    -- Automatically Login (Work in Progress) -- Trying to bypass login in screen not working at the moment
    Also put the database functionality into  a function because that massive else statement expanding the whole page was pissing me off, sorry Ashneel lol

--- Mobile/LoneSafe/app/src/main/java/io/github/itsjumaah/lonesafe/Login.java
+++ Mobile/LoneSafe/app/src/main/java/io/github/itsjumaah/lonesafe/Login.java
@@ -42,6 +42,7 @@ public class Login extends AppCompatActivity {
 
 
 
+
         assert btnLogin != null;
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,54 +59,9 @@ public class Login extends AppCompatActivity {
                     intent.putExtra("username", username);
                     intent.putExtra("name", name);
                     Login.this.startActivity(intent);
-                } else {
-                    //Get user data from DB
+                }
+                else { provokeDatabase(); }
 
-               // }
-
-                // Response received from the server
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-
-                        try {
-
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-
-
-
-                            if (success) {
-                                savePreferences();
-
-
-                                String name = jsonResponse.getString("name");
-                                int age = jsonResponse.getInt("age");
-
-                                Intent intent = new Intent(Login.this, Settings.class);
-                                intent.putExtra("name", name);
-                                intent.putExtra("age", age);
-                                intent.putExtra("username", username);
-                                Login.this.startActivity(intent);
-
-                            } else {
-                                AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
-                                builder.setMessage("Login Failed")
-                                        .setNegativeButton("Retry", null)
-                                        .create()
-                                        .show();
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                };
-
-                LoginRequest loginRequest = new LoginRequest(username, password, responseListener);
-                RequestQueue queue = Volley.newRequestQueue(Login.this);
-                queue.add(loginRequest);
-            }
             }
         });
 
@@ -162,21 +118,66 @@ public class Login extends AppCompatActivity {
         else { Toast.makeText(this,"Data Loaded Sucessfully",Toast.LENGTH_LONG).show();
             etUser.setText(Name);
             etPassword.setText(Password);
+            btnLogin.performClick();
+
         }
 
-        // et pass
+    }
+
+    public void provokeDatabase() {
+
+        final EditText etUser = (EditText) findViewById(R.id.etUser);
+        final EditText etPassword = (EditText) findViewById(R.id.etPassword);
+
+        final String username = etUser.getText().toString();
+        final String password = etPassword.getText().toString();
+        //Get user data from DB
+
+        // }
+
+        // Response received from the server
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                loadPreferences();
+                try {
+
+
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
 
 
 
+                    if (success) {
+                        savePreferences();
 
 
+                        String name = jsonResponse.getString("name");
+                        int age = jsonResponse.getInt("age");
 
+                        Intent intent = new Intent(Login.this, Settings.class);
+                        intent.putExtra("name", name);
+                        intent.putExtra("age", age);
+                        intent.putExtra("username", username);
+                        Login.this.startActivity(intent);
 
+                    } else {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+                        builder.setMessage("Login Failed")
+                                .setNegativeButton("Retry", null)
+                                .create()
+                                .show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
 
+            }
+        };
 
-
-
-
+        LoginRequest loginRequest = new LoginRequest(username, password, responseListener);
+        RequestQueue queue = Volley.newRequestQueue(Login.this);
+        queue.add(loginRequest);
 
 
     }
